408c12963faa8711f41fb03e29267af9131bb187
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-24 00:05:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,5 +8,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  rooms = []
+  attendees.each_with_index {|name, index| rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  rooms
 end
